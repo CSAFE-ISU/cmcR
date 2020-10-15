162068743377cdc0aaf46e7c225a1b60a7f65cb1
Joe Zemmels <jzemmels@iastate.edu>
2020-10-15 17:32:55 -0500

    Update processed data in package and begin new vignette

--- data-raw/fadul_examples.R
+++ data-raw/fadul_examples.R
@@ -1,28 +1,45 @@
 # code to prepare `fadul_examples` dataset goes here
-set.seed(4132020)
 
-fadul1.1_raw <- x3ptools::read_x3p("https://tsapps.nist.gov/NRBTD/Studies/CartridgeMeasurement/DownloadMeasurement/2d9cc51f-6f66-40a0-973a-a9292dbee36d") %>%
-  x3ptools::sample_x3p(m = 8)
+fadul1.1_raw <- x3ptools::read_x3p("https://tsapps.nist.gov/NRBTD/Studies/CartridgeMeasurement/DownloadMeasurement/2d9cc51f-6f66-40a0-973a-a9292dbee36d")
 
-fadul1.1_processed <- fadul1.1_raw
+fadul1.1_processed <- fadul1.1_raw %>%
+  cmcR::cropBFExterior(radiusOffset = -30,
+                       agg_function = median) %>%
+  cmcR::filterBFInterior(radiusOffset = 200) %>%
+  cmcR::levelByConditionalStatistic(statistic = "quantile",
+                                    tau = .5,
+                                    method = "fn") %>%
+  x3ptools::sample_x3p(m = 2)
 
-fadul1.1_processed$surface.matrix <- fadul1.1_processed$surface.matrix %>%
-  cmcR::preProcess_ransac() %>%
-  cmcR::preProcess_levelBF(useResiduals = TRUE)  %>%
-  cmcR::preProcess_cropWS(croppingThresh =  1)  %>%
-  cmcR::preProcess_removeFPCircle(aggregation_function = function(x,na.rm){min(x,na.rm = na.rm) - 15}) %>%
-  cmcR::preProcess_gaussFilter(res = fadul1.1_processed$header.info$incrementY)
+fadul1.1_processed$surface.matrix <- cmcR::preProcess_gaussFilter(surfaceMat = fadul1.1_processed$surface.matrix,
+                                                                  res = fadul1.1_processed$header.info$incrementY)
 
-fadul1.2_raw <- x3ptools::read_x3p("https://tsapps.nist.gov/NRBTD/Studies/CartridgeMeasurement/DownloadMeasurement/cb296c98-39f5-46eb-abff-320a2f5568e8") %>%
-  x3ptools::sample_x3p(m = 8)
+fadul1.2_raw <- x3ptools::read_x3p("https://tsapps.nist.gov/NRBTD/Studies/CartridgeMeasurement/DownloadMeasurement/cb296c98-39f5-46eb-abff-320a2f5568e8")
 
-fadul1.2_processed <- fadul1.2_raw
+fadul1.2_processed <- fadul1.2_raw %>%
+  cmcR::cropBFExterior(radiusOffset = -30,
+                       agg_function = median) %>%
+  cmcR::filterBFInterior(radiusOffset = 200) %>%
+  cmcR::levelByConditionalStatistic(statistic = "quantile",
+                                    tau = .5,
+                                    method = "fn") %>%
+  x3ptools::sample_x3p(m = 2)
 
-fadul1.2_processed$surface.matrix <- fadul1.2_processed$surface.matrix %>%
-  cmcR::preProcess_ransac() %>%
-  cmcR::preProcess_levelBF(useResiduals = TRUE)  %>%
-  cmcR::preProcess_cropWS(croppingThresh =  1)  %>%
-  cmcR::preProcess_removeFPCircle(aggregation_function = function(x,na.rm){min(x,na.rm = na.rm) - 15}) %>%
-  cmcR::preProcess_gaussFilter(res = fadul1.2_processed$header.info$incrementY)
+fadul1.2_processed$surface.matrix <- cmcR::preProcess_gaussFilter(surfaceMat = fadul1.2_processed$surface.matrix,
+                                                                  res = fadul1.2_processed$header.info$incrementY)
 
-usethis::use_data(fadul1.1_raw,fadul1.1_processed,fadul1.2_raw,fadul1.2_processed, overwrite = TRUE)
+fadul2.1_raw <- x3ptools::read_x3p("https://tsapps.nist.gov/NRBTD/Studies/CartridgeMeasurement/DownloadMeasurement/8ae0b86d-210a-41fd-ad75-8212f9522f96")
+
+fadul2.1_processed <- fadul2.1_raw %>%
+  cmcR::cropBFExterior(radiusOffset = -30,
+                       agg_function = median) %>%
+  cmcR::filterBFInterior(radiusOffset = 200) %>%
+  cmcR::levelByConditionalStatistic(statistic = "quantile",
+                                    tau = .5,
+                                    method = "fn") %>%
+  x3ptools::sample_x3p(m = 2)
+
+fadul2.1_processed$surface.matrix <- cmcR::preProcess_gaussFilter(surfaceMat = fadul2.1_processed$surface.matrix,
+                                                                  res = fadul2.1_processed$header.info$incrementY)
+
+usethis::use_data(fadul1.1_processed,fadul1.2_processed, fadul2.1_processed,overwrite = TRUE)
